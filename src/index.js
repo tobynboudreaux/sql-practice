f93be73a6c77074e512458d5b05ae24d83da1a74
tobynboudreaux <tobynboudreaux@gmail.com>
2020-10-13 15:39:15 -0500

    add config, .env, and update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 "use strict";
 
+const config = require("./config");
 const server = require("./server");
 
 const startServer = async () => {
   try {
-    // todo: move configuration to separate config
-    const config = {
-      host: "localhost",
-      port: 8080,
-    };
-
     // create an instance of the server application
     const app = await server(config);
 
