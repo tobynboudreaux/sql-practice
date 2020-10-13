ae96e679d7b4b250ad0433be00685c47839f8943
tobynboudreaux <tobynboudreaux@gmail.com>
2020-10-13 15:36:22 -0500

    first commit'

---
+++ db.sql
@@ -0,0 +1,21 @@
+USE master;
+GO
+
+CREATE DATABASE calendar; -- change this to whatever database name you desire
+GO
+
+-- Dropping events table...
+DROP TABLE IF EXISTS events;
+
+-- Create events table...
+CREATE TABLE events (
+   id int IDENTITY(1, 1) PRIMARY KEY CLUSTERED NOT NULL
+   , userId nvarchar(50) NOT NULL
+   , title nvarchar(200) NOT NULL
+   , description nvarchar(1000) NULL
+   , startDate date NOT NULL
+   , startTime time(0) NULL
+   , endDate date NULL
+   , endTime time(0) NULL
+   , INDEX idx_events_userId ( userId )
+);
